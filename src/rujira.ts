7a12e9a789ac081fcdfd73391f29a64755f8c362
Danilo Araújo Silva <danilo.silva@funttastic.com>
2025-07-10 18:14:37 -0300

    Implement Rujira class and define interfaces for FIN Protocol requests and responses

---
+++ src/rujira.ts
@@ -0,0 +1,21 @@
+export class Rujira {
+	private readonly fin: Fin;
+
+	constructor() {
+		this.fin = undefined as unknown as Fin;
+	}
+}
+
+export interface FinGetTokenRequest {}
+export interface FinGetTokenResponse {}
+export interface FinGetTokensRequest {}
+export interface FinGetTokensResponse {}
+export interface FinGetBalancesRequest {}
+export interface FinGetBalancesResponse {}
+export interface FinGetOrdersRequest {}
+export interface FinGetOrdersResponse {}
+export interface FinGetOrderBookRequest {}
+
+export class Fin {
+
+}
