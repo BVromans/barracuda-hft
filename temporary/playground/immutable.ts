108bf8f2306de29bea1d01e645a31d27c168f38e
Danilo Araújo Silva <danilo.silva@funttastic.com>
2025-07-25 17:09:33 -0300

    Refactor playground examples in immutable.ts to enhance clarity and functionality
    - Updated the instantiation of Map and List to use explicit type annotations for better type safety.
    - Added new examples demonstrating the usage of List, including pushing new elements and retrieving values.
    - Removed commented-out code to streamline the file and improve readability.

--- temporary/playground/immutable.ts
+++ temporary/playground/immutable.ts
@@ -1,18 +1,6 @@
-import { makeCompactBitArray } from '@cosmjs/stargate/build/multisignature';
 import { List, Map } from '../../src/extensions/immutablejs/types';
-import { has } from 'immutable';
 
-// const list = List<number>([1, 2, 3]);
-
-// console.log(list.get(0));
-// console.log(list.get(1));
-// console.log(list.get(2));
-// console.log(list.get(3));
-// console.log(list.get(4));
-// console.log(list.get(5));
-// console.log(list.get(6));
-
-const map = Map<string, number>({ 'a': 1, 'b': 2, 'c': 3 });
+const map: Map<string, number> = Map<string, number>({ 'a': 1, 'b': 2, 'c': 3 });
 
 map.set('d', 4);
 map.set('e', 5);
@@ -36,3 +24,22 @@ console.log(map.toJS());
 
 console.log(map.getIn(['h', 'i']));
 console.log(map.get('h.j'));
+
+
+const list: List<number> = List<number>([1, 2, 3]);
+
+list.push(4);
+list.push(5);
+list.push(6);
+list.push(7);
+
+console.log(list.toJS());
+
+console.log(list.get(0));
+console.log(list.get(1));
+console.log(list.get(2));
+console.log(list.get(3));
+console.log(list.get(4));
+console.log(list.get(5));
+console.log(list.get(6));
+console.log(list.get(7));
