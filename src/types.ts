5eefe91c749856a31250f90fa33ecbd5a2e13f36
guilherme.barbosa <guilherme.barbosa@funttastic.com>
2025-08-13 10:53:13 -0300

    Refactor Indicator class to streamline indicator listing
    - Removed commented-out indicators and replaced them with active entries in the getAll method for clarity and maintainability.
    - Updated the get-indicators.ts file to include documentation notes for various indicators, enhancing understanding of their status and documentation needs.

--- src/types.ts
+++ src/types.ts
@@ -1492,101 +1492,101 @@ export class Indicator {
 	 */
 	static getAll(): Indicator[] {
 		return [
-			// Indicator.abands, ok
-			// Indicator.absolute_price_oscillator, ok
-			// Indicator.accumulation_distribution_line, ok
-			// Indicator.annualized_historical_volatility, ok
-			// Indicator.aroon, ok
-			// Indicator.aroon_oscillator, ok
-			// Indicator.arnaud_legoux_moving_average, ok
-			// Indicator.average_directional_movement_index, //ok
-			// Indicator.average_directional_movement_rating, ok
-			// Indicator.average_price, ok
-			// Indicator.average_true_range, ok
-			// Indicator.awesome_oscillator, ok
-			// Indicator.balance_of_power, ok
-			// Indicator.bollinger_bands, ok
-			// Indicator.chandelier_exit, ok
-			// Indicator.chaikin_money_flow, ok
-			// Indicator.chaikins_volatility, ok
-			// Indicator.chande_momentum_oscillator, ok
-			// Indicator.commodity_channel_index, ok
-			// Indicator.crossover, //ok
-			// Indicator.cross_over_number, //ok
-			// Indicator.cross_under_number, //ok
-			// Indicator.detrended_price_oscillator, ok
-			// Indicator.directional_indicator, ok
-			// Indicator.directional_movement, ok
-			// Indicator.directional_movement_index, ok
-			// Indicator.donchian_channels, ok
-			// Indicator.double_exponential_moving_average, ok
-			// Indicator.ease_of_movement, ok
-			// Indicator.exponential_decay, ok
-			// Indicator.exponential_moving_average, ok
-			// Indicator.fisher_transform, ok
-			// Indicator.force_index, ok
-			// Indicator.forecast_oscillator, ok
-			// Indicator.hull_moving_average,
-			// Indicator.kaufman_adaptive_moving_average, ok
-			// Indicator.keltner_channels, ok
-			// Indicator.klinger_volume_oscillator, ok
-			// Indicator.know_sure_thing, ok
-			// Indicator.lag, ok
-			// Indicator.linear_decay, ok
-			// Indicator.linear_regression, ok
-			// Indicator.linear_regression_intercept, ok
-			// Indicator.linear_regression_slope, ok
-			// Indicator.mass_index, //ok
-			// Indicator.market_facilitation_index, ok
-			// Indicator.maximum_in_period, ok
-			// Indicator.mean_deviation_over_period, ok
-			// Indicator.median_price, ok
-			// Indicator.minimum_in_period, ok
-			// Indicator.momentum, ok
-			// Indicator.money_flow_index, ok
-			// Indicator.moving_average_convergence_divergence, ok
-			// Indicator.negative_volume_index, ok
-			// Indicator.normalized_average_true_range, ok
-			// Indicator.on_balance_volume, ok
-			// Indicator.pbands, ok
-			// Indicator.parabolic_sar, ok
-			// Indicator.percentage_price_oscillator, ok
-			// Indicator.polarized_fractal_efficiency, ok
-			// Indicator.poscillator, ok
-			// Indicator.positive_volume_index, ok
-			// Indicator.qstick, ok
-			// Indicator.rate_of_change, ok
-			// Indicator.rate_of_change_ratio, ok
-			// Indicator.recursive_moving_trend_average, ok
-			// Indicator.relative_momentum_index,  //ok
-			// Indicator.relative_strength_index, ok
-			// Indicator.relative_vigor_index, ok
-			// Indicator.simple_moving_average, ok
-			// Indicator.standard_deviation_over_period, ok
-			// Indicator.standard_error_over_period, ok
-			// Indicator.stochastic_momentum_index, //ok
-			// Indicator.stochastic_oscillator, //ok
-			// Indicator.stochastic_rsi, //ok
-			// Indicator.sum_over_period, ok
-			// Indicator.time_series_forecast, ok
-			// Indicator.triangular_moving_average, ok
-			// Indicator.trix,  //ok
-			// Indicator.true_range, //ok
-			// Indicator.true_strength_index, ok
-			// Indicator.triple_exponential_moving_average, ok
-			// Indicator.typical_price, ok
-			// Indicator.ultimate_oscillator, ok
-			// Indicator.variable_index_dynamic_average, //ok
-			// Indicator.vertical_horizontal_filter, ok
-			// Indicator.volume_oscillator, ok
-			// Indicator.volume_weighted_average_price, ok
-			// Indicator.volume_weighted_moving_average, ok
-			// Indicator.weighted_close_price, ok
-			// Indicator.weighted_moving_average, ok
-			// Indicator.williams_accumulation_distribution, ok
-			// Indicator.williams_r, //ok
-			// Indicator.wilders_smoothing, ok
-			// Indicator.zero_lag_exponential_moving_average, ok
+			Indicator.abands,
+			Indicator.absolute_price_oscillator,
+			Indicator.accumulation_distribution_line,
+			Indicator.annualized_historical_volatility,
+			Indicator.aroon,
+			Indicator.aroon_oscillator,
+			Indicator.arnaud_legoux_moving_average,
+			Indicator.average_directional_movement_index,
+			Indicator.average_directional_movement_rating,
+			Indicator.average_price,
+			Indicator.average_true_range,
+			Indicator.awesome_oscillator,
+			Indicator.balance_of_power,
+			Indicator.bollinger_bands,
+			Indicator.chandelier_exit,
+			Indicator.chaikin_money_flow,
+			Indicator.chaikins_volatility,
+			Indicator.chande_momentum_oscillator,
+			Indicator.commodity_channel_index,
+			Indicator.crossover,
+			Indicator.cross_over_number,
+			Indicator.cross_under_number,
+			Indicator.detrended_price_oscillator,
+			Indicator.directional_indicator,
+			Indicator.directional_movement,
+			Indicator.directional_movement_index,
+			Indicator.donchian_channels,
+			Indicator.double_exponential_moving_average,
+			Indicator.ease_of_movement,
+			Indicator.exponential_decay,
+			Indicator.exponential_moving_average,
+			Indicator.fisher_transform,
+			Indicator.force_index,
+			Indicator.forecast_oscillator,
+			Indicator.hull_moving_average,
+			Indicator.kaufman_adaptive_moving_average,
+			Indicator.keltner_channels,
+			Indicator.klinger_volume_oscillator,
+			Indicator.know_sure_thing,
+			Indicator.lag,
+			Indicator.linear_decay,
+			Indicator.linear_regression,
+			Indicator.linear_regression_intercept,
+			Indicator.linear_regression_slope,
+			Indicator.mass_index,
+			Indicator.market_facilitation_index,
+			Indicator.maximum_in_period,
+			Indicator.mean_deviation_over_period,
+			Indicator.median_price,
+			Indicator.minimum_in_period,
+			Indicator.momentum,
+			Indicator.money_flow_index,
+			Indicator.moving_average_convergence_divergence,
+			Indicator.negative_volume_index,
+			Indicator.normalized_average_true_range,
+			Indicator.on_balance_volume,
+			Indicator.pbands,
+			Indicator.parabolic_sar,
+			Indicator.percentage_price_oscillator,
+			Indicator.polarized_fractal_efficiency,
+			Indicator.poscillator,
+			Indicator.positive_volume_index,
+			Indicator.qstick,
+			Indicator.rate_of_change,
+			Indicator.rate_of_change_ratio,
+			Indicator.recursive_moving_trend_average,
+			Indicator.relative_momentum_index,
+			Indicator.relative_strength_index,
+			Indicator.relative_vigor_index,
+			Indicator.simple_moving_average,
+			Indicator.standard_deviation_over_period,
+			Indicator.standard_error_over_period,
+			Indicator.stochastic_momentum_index,
+			Indicator.stochastic_oscillator,
+			Indicator.stochastic_rsi,
+			Indicator.sum_over_period,
+			Indicator.time_series_forecast,
+			Indicator.triangular_moving_average,
+			Indicator.trix,
+			Indicator.true_range,
+			Indicator.true_strength_index,
+			Indicator.triple_exponential_moving_average,
+			Indicator.typical_price,
+			Indicator.ultimate_oscillator,
+			Indicator.variable_index_dynamic_average,
+			Indicator.vertical_horizontal_filter,
+			Indicator.volume_oscillator,
+			Indicator.volume_weighted_average_price,
+			Indicator.volume_weighted_moving_average,
+			Indicator.weighted_close_price,
+			Indicator.weighted_moving_average,
+			Indicator.williams_accumulation_distribution,
+			Indicator.williams_r,
+			Indicator.wilders_smoothing,
+			Indicator.zero_lag_exponential_moving_average,
 		];
 	}
 }
--- temporary/playground/working/get-indicators.ts
+++ temporary/playground/working/get-indicators.ts
@@ -72,3 +72,99 @@ import { Candle, List, RujiraConstructorOptions, RujiraInitializeOptions, Wallet
 
 	console.log(indicators.toJS());
 })();
+
+// Indicator.abands, ok no doc
+			// Indicator.absolute_price_oscillator, ok no doc
+			// Indicator.accumulation_distribution_line, ok
+			// Indicator.annualized_historical_volatility, ok (Historical Volatility)
+			// Indicator.aroon, ok
+			// Indicator.aroon_oscillator, ok no doc
+			// Indicator.arnaud_legoux_moving_average, ok
+			// Indicator.average_directional_movement_index, //ok (Average Directional Index)
+			// Indicator.average_directional_movement_rating, ok no doc
+			// Indicator.average_price, ok no doc
+			// Indicator.average_true_range, ok
+			// Indicator.awesome_oscillator, ok
+			// Indicator.balance_of_power, ok
+			// Indicator.bollinger_bands, ok
+			// Indicator.chandelier_exit, ok no doc
+			// Indicator.chaikin_money_flow, ok
+			// Indicator.chaikins_volatility, //ok no doc
+			// Indicator.chande_momentum_oscillator, ok
+			// Indicator.commodity_channel_index, ok
+			// Indicator.crossover, //ok no doc
+			// Indicator.cross_over_number, //ok no doc
+			// Indicator.cross_under_number, //ok no doc
+			// Indicator.detrended_price_oscillator, //ok
+			// Indicator.directional_indicator, ok no doc
+			// Indicator.directional_movement, ok
+			// Indicator.directional_movement_index, ok
+			// Indicator.donchian_channels, ok
+			// Indicator.double_exponential_moving_average, ok
+			// Indicator.ease_of_movement, ok
+			// Indicator.exponential_decay, //ok no doc
+			// Indicator.exponential_moving_average, ok
+			// Indicator.fisher_transform, ok
+			// Indicator.force_index, //ok (Elder's Force Index (EFI))
+			// Indicator.forecast_oscillator, //ok no doc
+			// Indicator.hull_moving_average, //ok
+			// Indicator.kaufman_adaptive_moving_average, //ok no doc
+			// Indicator.keltner_channels, //ok no doc
+			// Indicator.klinger_volume_oscillator, ok
+			// Indicator.know_sure_thing, //ok
+			// Indicator.lag, ok no doc
+			// Indicator.linear_decay, //ok no doc
+			// Indicator.linear_regression, //ok
+			// Indicator.linear_regression_intercept, //ok no doc
+			// Indicator.linear_regression_slope, //ok no doc
+			// Indicator.mass_index, //ok
+			// Indicator.market_facilitation_index, //ok no doc
+			// Indicator.maximum_in_period, //ok no doc
+			// Indicator.mean_deviation_over_period, //ok no doc
+			// Indicator.median_price, //ok no doc
+			// Indicator.minimum_in_period, //ok no doc
+			// Indicator.momentum, //ok no doc
+			// Indicator.money_flow_index, //ok
+			// Indicator.moving_average_convergence_divergence, //ok
+			// Indicator.negative_volume_index, //ok no doc
+			// Indicator.normalized_average_true_range, //ok no doc
+			// Indicator.on_balance_volume, //ok
+			// Indicator.pbands, //ok no doc
+			// Indicator.parabolic_sar, //ok no doc
+			// Indicator.percentage_price_oscillator, //ok no doc
+			// Indicator.polarized_fractal_efficiency, //ok no doc
+			// Indicator.poscillator, //ok no doc
+			// Indicator.positive_volume_index, //ok no doc
+			// Indicator.qstick, //ok no doc
+			// Indicator.rate_of_change, //ok no doc
+			// Indicator.rate_of_change_ratio, //ok no doc
+			// Indicator.recursive_moving_trend_average, //ok no doc
+			// Indicator.relative_momentum_index,  //ok no doc
+			// Indicator.relative_strength_index, //ok
+			// Indicator.relative_vigor_index, //ok
+			// Indicator.simple_moving_average, //ok no doc
+			// Indicator.standard_deviation_over_period, //ok no doc
+			// Indicator.standard_error_over_period, //ok no doc
+			// Indicator.stochastic_momentum_index, //ok
+			// Indicator.stochastic_oscillator, //ok
+			// Indicator.stochastic_rsi, //ok
+			// Indicator.sum_over_period, //ok no doc
+			// Indicator.time_series_forecast, //ok no doc
+			// Indicator.triangular_moving_average, //ok no doc
+			// Indicator.trix,  //ok
+			// Indicator.true_range, //ok no doc
+			// Indicator.true_strength_index, //ok
+			// Indicator.triple_exponential_moving_average, //ok
+			// Indicator.typical_price, //ok no doc
+			// Indicator.ultimate_oscillator, //ok
+			// Indicator.variable_index_dynamic_average, //ok no doc
+			// Indicator.vertical_horizontal_filter, //ok no doc
+			// Indicator.volume_oscillator, //ok
+			// Indicator.volume_weighted_average_price, //ok
+			// Indicator.volume_weighted_moving_average, //ok
+			// Indicator.weighted_close_price, //ok no doc
+			// Indicator.weighted_moving_average, //ok
+			// Indicator.williams_accumulation_distribution, //ok no doc
+			// Indicator.williams_r, //ok
+			// Indicator.wilders_smoothing, //ok no doc
+			// Indicator.zero_lag_exponential_moving_average, //ok no doc
