83de2b8b48f00fab40c2d3f2455a1c5c1fc7714c
Danilo Araújo Silva <danilo.silva@funttastic.com>
2025-08-14 19:40:02 -0300

    Enhance logger functionality with detailed stack trace and additional log levels
    - Implemented a custom stack trace preparation method to enrich error logging with detailed call site information.
    - Added new logging methods for different log levels: debug, info, warning, error, and critical, improving the flexibility of logging throughout the application.
    - Updated the log method to include timestamp, file path, line number, and function/method name in log messages for better traceability.

--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,59 @@
+Error.prepareStackTrace = (err, stack) => {
+	return stack.map(callSite => {
+		// getThis	this value of the function call
+		// getTypeName	typeof this
+		// getFunction	function object
+		// getFunctionName	function name as a string
+		// getMethodName	method name as a string
+		// getFileName	file name or URL
+		// getLineNumber	line number
+		// getColumnNumber	column number
+		// getEvalOrigin	undefined
+		// getScriptNameOrSourceURL	source URL
+		// isToplevel	returns true if the function is in the global scope
+		// isEval	returns true if the function is an eval call
+		// isNative	returns true if the function is native
+		// isConstructor	returns true if the function is a constructor
+		// isAsync	returns true if the function is async
+		// isPromiseAll	Not implemented yet.
+		// getPromiseIndex	Not implemented yet.
+		// toString
+		const result = {
+			this: callSite.getThis(),
+			typeName: callSite.getTypeName(),
+			function: callSite.getFunction(),
+			functionName: callSite.getFunctionName(),
+			methodName: callSite.getMethodName(),
+			fileName: callSite.getFileName(),
+			lineNumber: callSite.getLineNumber(),
+			columnNumber: callSite.getColumnNumber(),
+			evalOrigin: callSite.getEvalOrigin(),
+			scriptNameOrSourceURL: callSite.getScriptNameOrSourceURL(),
+			isToplevel: callSite.isToplevel(),
+			isEval: callSite.isEval(),
+			isNative: callSite.isNative(),
+			isConstructor: callSite.isConstructor(),
+			isAsync: callSite.isAsync(),
+			isPromiseAll: callSite.isPromiseAll(),
+			promiseIndex: callSite.getPromiseIndex(),
+			string: callSite.toString(),
+		}
+
+		return result;
+	});
+};
+
+
+/**
+ * Log levels
+ */
+export enum LogLevel {
+	DEBUG = 'debug',
+	INFO = 'info',
+	WARNING = 'warning',
+	ERROR = 'error',
+	CRITICAL = 'critical',
+}
 
 /**
  * Centralized, singleton application logger.
@@ -26,19 +82,90 @@ export class Logger {
 	}
 
 	/**
+	 * Log a debug message
+	 * @param message - The message to log
+	 */
+	public debug(message: string, ...optionalParams: any[]): void {
+		this.log(LogLevel.DEBUG, message, ...optionalParams);
+	}
+
+	/**
+	 * Log an information
+	 * @param message - The message to log
+	 */
+	public info(message: string, ...optionalParams: any[]): void {
+		this.log(LogLevel.INFO, message, ...optionalParams);
+	}
+
+	/**
+	 * Log a warning
+	 * @param message - The message to log
+	 */
+	public warning(message: string, ...optionalParams: any[]): void {
+		this.log(LogLevel.WARNING, message, ...optionalParams);
+	}
+
+	/**
+	 * Log an error
+	 * @param message - The message to log
+	 */
+	public error(message: string, ...optionalParams: any[]): void {
+		this.log(LogLevel.ERROR, message, ...optionalParams);
+	}
+
+	/**
+	 * Log a critical message
+	 * @param message - The message to log
+	 */
+	public critical(message: string, ...optionalParams: any[]): void {
+		this.log(LogLevel.CRITICAL, message, ...optionalParams);
+	}
+
+	/**
+	 * Log a message
+	 * @param message - The message to log
+	 */
+	public log(level: LogLevel, message: string, ...optionalParams: any[]): void {
+		const timestamp = new Date().toISOString();
+		const stack = new Error().stack as any;
+		const frame = stack[2];
+		const filePath = frame.fileName;
+		const lineNumber = frame.lineNumber;
+		const columnNumber = frame.columnNumber;
+		const functionName = frame.functionName;
+		const methodName = frame.methodName;
+
+		const stacktrace = stack.map((frame: any) => frame.string).join('\n');
+
+		message = `[${timestamp}][${level}][${filePath}:${lineNumber}:${columnNumber}][${functionName || methodName}]: ${message}\n\n${stacktrace}`;
+
+		if (level === LogLevel.DEBUG) {
+			console.debug(message, ...optionalParams);
+		} else if (level === LogLevel.INFO) {
+			console.info(message, ...optionalParams);
+		} else if (level === LogLevel.WARNING) {
+			console.warn(message, ...optionalParams);
+		} else if (level === LogLevel.ERROR) {
+			console.error(message, ...optionalParams);
+		} else if (level === LogLevel.CRITICAL) {
+			console.error(message, ...optionalParams);
+		}
+	}
+
+	/**
 	 * Ignore an exception
 	 * @param exception - The exception to ignore
 	 * @param message - The message to log
 	 */
 	public ignoreException(exception: any, message?: string): void {
-		message = message || 'Ignored exception: ';
+		message = message || 'Ignored exception:';
 		if (exception instanceof Error) {
-			message += `\n${exception.message}\n${exception.stack}`;
+			message += ` ${exception.message}`;
 		} else {
 			message += exception;
 		}
 
-		console.warn(message);
+		this.log(LogLevel.WARNING, message);
 	}
 }
 
