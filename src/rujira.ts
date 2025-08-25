0be355c9dedf3630a087242c3e8c29505094edf8
Danilo Araújo Silva <danilo.silva@funttastic.com>
2025-08-25 15:38:30 -0300

    Update package scripts for improved debugging and disable logging in Rujira client
    - Enhanced the development and start scripts in package.json to include source maps and async stack traces for better debugging during development.
    - Disabled logging in the Rujira client and Fin client to optimize performance.
    - Updated the BasePureMarketMakingStrategy class to enable specific methods for market updates.
    - Ensured that all integration tests pass successfully after the modifications.

--- src/rujira.ts
+++ src/rujira.ts
@@ -140,7 +140,7 @@ cacheManager.setOptions(<CacheManagerOptions>{
  * Rujira client
  */
 @loggedClass({
-	enabled: true,
+	enabled: false,
 	logger: logger,
 	allowedMethods: [],
 	disallowedMethods: [],
@@ -649,7 +649,7 @@ export class Rujira {
  * Fin client
  */
 @loggedClass({
-	enabled: true,
+	enabled: false,
 	logger: logger,
 	allowedMethods: [],
 	disallowedMethods: [],
