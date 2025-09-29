1905103bf56a85b941ff379366509baecf1f554f
Danilo Araújo Silva <danilo.silva@funttastic.com>
2025-09-29 14:18:04 -0300

    Update profit and loss calculation in BasePureMarketMakingStrategy to use percentage change
    - Modified the profit and loss calculation to retrieve the percentage change instead of the absolute total balance change, enhancing the accuracy of wallet loss monitoring.
    - Confirmed that all integration tests pass successfully after these modifications.

--- src/strategies/base_pure_market_making_strategy.ts
+++ src/strategies/base_pure_market_making_strategy.ts
@@ -564,7 +564,7 @@ export abstract class BasePureMarketMakingStrategy implements BaseStrategy {
 			if (enabled) {
 				const maximumAllowedWalletLossFromInitialValue = Decimal(properties.getAs<number>('strategy.pure_market_making.common.monitorProfitAndLoss.maximumAllowedWalletLossFromInitialValue'));
 
-				const currentToInitialProfitAndLoss: Decimal = this.state.getOrThrow('summary.profitAndLoss.currentToInitial.totalBalanceChange.absolute');
+				const currentToInitialProfitAndLoss: Decimal = this.state.getOrThrow('summary.profitAndLoss.currentToInitial.totalBalanceChange.percentage');
 
 				if (currentToInitialProfitAndLoss.lte(maximumAllowedWalletLossFromInitialValue.neg())) {
 					this.status = StrategyStatus.STOP_REQUESTED;
